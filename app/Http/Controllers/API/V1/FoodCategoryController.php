de929f701b45bd77cccab3557a5487f84429ddf2
csik-patrik <csi.patrik455@gmail.com>
2023-03-01 19:04:55 +0100

    Implement FoodCategoryController

--- app/Http/Controllers/API/V1/FoodCategoryController.php
+++ app/Http/Controllers/API/V1/FoodCategoryController.php
@@ -3,62 +3,73 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\UpdateFoodCategoryRequest;
+use App\Http\Resources\V1\FoodCategoryCollection;
+use App\Http\Resources\V1\FoodCategoryResource;
+use App\Models\FoodCategory;
 use Illuminate\Http\Request;
+use App\Filters\V1\FoodCategoryFilter;
+use App\Http\Requests\V1\StoreFoodCategoryRequest;
 
 class FoodCategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return FoodCategoryCollection
+     *
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $filter = new FoodCategoryFilter();
+
+        $filterItems = $filter->transform($request);
+
+        $foodCategories = FoodCategory::where($filterItems);
+
+        return new FoodCategoryCollection($foodCategories->paginate()->appends($request->query()));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  StoreFoodCategoryRequest  $request
+     * @return FoodCategoryResource
      */
-    public function store(Request $request)
+    public function store(StoreFoodCategoryRequest $request)
     {
-        //
+        return new FoodCategoryResource(FoodCategory::create($request->all()));
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  FoodCategory $foodCategory
+     * @return FoodCategoryResource
      */
-    public function show($id)
+    public function show(FoodCategory $foodCategory)
     {
-        //
+        return new FoodCategoryResource($foodCategory);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  UpdateFoodCategoryRequest  $request
+     * @param  FoodCategory $foodCategory
      */
-    public function update(Request $request, $id)
+    public function update(UpdateFoodCategoryRequest $request, FoodCategory $foodCategory)
     {
-        //
+        $foodCategory->update($request->all());
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  FoodCategory $foodCategory
      */
-    public function destroy($id)
+    public function destroy(FoodCategory $foodCategory)
     {
-        //
+        $foodCategory->delete();
     }
 }
